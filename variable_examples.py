9f94757631dcf5326e5eb0438979a86ef510bdf3
JaeJae04 <jjohnson@appdev.yearup.org>
2024-10-23 21:11:06 -0500

    Add files via upload

---
+++ variable_examples.py
@@ -0,0 +1,56 @@
+CustomerID = 8
+# integers #
+FirstName = 'Leila'
+ # names gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+MiddleName = 'Marie'
+ # names gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+LastName = 'Malcom'
+ # names  gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+Gender = 'Female'
+ # names gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+DOB = '11/1/2008'
+ # names gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+Dln = 'D400-7836-001'
+ # names gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+AutoPolicyNumber = '123-4567-8910'
+
+ # names gender are sting type valvues and its most commonly that dates are stored as sting values#
+
+print(FirstName + ' ' + Gender)
+
+
+City = 'Chicago'
+
+State = 'Illinois'
+
+print(City + ', ' + State )
+
+
+FirstName = 'Jaela'
+
+LastName = 'Johnson'
+
+MiddleName = 'Diamond'
+
+City = 'chicago'
+
+State = 'Illinois'
+ 
+ # Lab 2 
+
+ # What is the full list of reserved words that can't be used for variable names?
+
+'''  None, True, and, as, assert, break, class, continue, def, del, elif, else, except, finally, for, from, global,
+if, import, in, is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield'''
+
+# 1. ^def^ - used to define a function or method in python.
+# 2. ^false^ - a boolean operator that represents the oppsite of true.
+# 3. ^return^ - used to exit a function or a method and return some value.
+# 4. ^in^ - used to check if an element is present in an iterable like list or tuple.
+# 5. ^while^ - used to start a while loop. it continues iteration untile a condition is no longer true.
